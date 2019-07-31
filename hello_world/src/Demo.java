cf093e2bb6671388ba6420112770e6526e7d1d51
ye <xphouziyu@qq.com>
2019-08-01 00:52:42 +0800

    第一次写JAVA

---
+++ hello_world/src/Demo.java
@@ -0,0 +1,16 @@
+public class Demo {
+    /*
+    main 方法的快捷方式    psvm
+
+    输出语句的快捷方式     sout
+     */
+
+    public static void main(String[] args) {
+        System.out.println("我的小伙伴");
+        System.out.println("houziyu");
+        System.out.println(123);
+        System.out.println(3.14);
+        System.out.println('a');
+        System.out.println("a");
+    }
+}
---
+++ hello_world/src/Demo2.java
@@ -0,0 +1,38 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        //byte 类型
+        byte b = 1;
+        System.out.println(b);
+
+        //short 类型
+        short s = 12;
+        System.out.println(s);
+
+        //int 类型
+        int i = 123456;
+        System.out.println(i);
+
+        //long 类型
+        long l = 1234567L;
+        System.out.println(l);
+
+        //float 类型
+        float f = 0.1F;
+        System.out.println(f);
+
+        //double 类型
+        double d = 3.14;
+        System.out.println(d);
+
+        //char 类型
+        char c = 'a';
+        System.out.println(c);
+
+        //boolean 类型
+        boolean bb = true;
+        System.out.println(bb);
+    }
+
+}
